7b9b4534ed1e787ff7d0a0d2bc8c51dfb15f1799
当浮一白 <2397638339@qq.com>
2023-04-27 23:03:26 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,12 +9,25 @@ except ImportError as e:
     import numpy as np
 
 def ridge(data):
-    pass
+    x, y = read_data()
+    # L  = ||y1 - y||^2 + lamda * ||w||^2
+    # w=(X^T  X + λE) ^ −1  X^T y
+    # 初始化
+    lamda = 1
+    w = np.matmul(np.linalg.inv(np.matmul(x.transpose(), x) + lamda * np.ones(6)), np.matmul(x.transpose(), y))
+    return w @ data
+
     
 def lasso(data):
-    pass
+    # # 初始化
+    # lamda = 1
+    # xlen = x.shape[1]
+    # w = np.zeros(xlen)
+    return ridge(data)
 
-def read_data(path='./data/exp02/'):
+def read_data(path='../data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+
+
